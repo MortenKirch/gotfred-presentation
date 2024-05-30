6476234445ef74db3ed095e0989065467ed5f368
RuneGSoerensen <eaa23rgso@students.eaaa.dk>
2024-05-30 13:22:25 +0200

    Major changes
    morten er dum

--- javascript/image-swap-forside-cafe.js
+++ javascript/image-swap-forside-cafe.js
@@ -1,10 +1,9 @@
-"use strikt"
 // DOMContentLoaded, sørger for at den loader content på siden først
 document.addEventListener("DOMContentLoaded", function() {
 //    forskellige billede sources i et array,
-    let images = ["/pictures/bryllupskage.jpg", "/pictures/billede-catering-2.png", "/pictures/billede-catering-3.png"];
+    let images = ["pictures/cafe-1.webp", "pictures/cafe-2.webp", "pictures/cafe-3.webp", "pictures/cafe-4.webp"];
     let currentIndex = 0;
-    let currentImage = document.getElementById('catering-image');
+    let currentImage = document.getElementById('gotfred-cafe-image');
 
     function changeImage() {
         // Fade out  current image
@@ -20,6 +19,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 1000); // css transtision duration/tid
     }
     // længden mellem hvert image swap, 5000ms/5 sek
-    setInterval(changeImage, 8000);
+    setInterval(changeImage, 5000);
     
 });
